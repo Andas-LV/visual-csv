126d8bbc013fe1be640cab76b89dc0c5132f12ec
Andas-LV <andassatybaldy@gmail.com>
2026-02-15 18:28:06 +0500

    experimental: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,15 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
 	...compat.extends("next/core-web-vitals", "next/typescript"),
-	{
-		rules: {
-			"@typescript-eslint/no-var-requires": "off",
-			"@typescript-eslint/no-unused-vars": "off",
-			"react/display-name": "off",
-			"@typescript-eslint/no-empty-object-type": "off",
-			"@typescript-eslint/no-explicit-any": "off",
-		},
-	},
+	// {
+	// 	rules: {
+	// 		"@typescript-eslint/no-var-requires": "off",
+	// 		"@typescript-eslint/no-unused-vars": "off",
+	// 		"react/display-name": "off",
+	// 		"@typescript-eslint/no-empty-object-type": "off",
+	// 		"@typescript-eslint/no-explicit-any": "off",
+	// 	},
+	// },
 ];
 
 export default eslintConfig;
