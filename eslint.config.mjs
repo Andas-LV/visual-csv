cf25834c3f1c3ded79950929432377c2a42881e0
Andas-LV <andassatybaldy@gmail.com>
2026-02-15 18:12:46 +0500

    fix: eslint.conf

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,15 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-	...compat.extends(
-		"next/core-web-vitals",
-		"next/typescript",
-		"@typescript-eslint/no-var-requires",
-	),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript"
+  ),
+  {
+    rules: {
+      "@typescript-eslint/no-var-requires": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
